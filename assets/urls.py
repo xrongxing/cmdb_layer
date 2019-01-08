fa8893c6d20811b94e06c1350b8f62f4fa1572bf
Sam <Sam@example.com>
2019-01-08 16:30:01 +0800

    index.html add assets list table, use dataTables plugins

--- assets/urls.py
+++ assets/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^report/', assets_views.report, name = 'report'),
     url(r'^dashboard/', assets_views.dashboard, name = 'dashboard'),
     url(r'^index/', assets_views.index, name = 'index'),
-    url(r'^detail/(?P<asset_id>[0-9]+)/$', assets_views.index, name = 'detail'),
+    url(r'^detail/(?P<asset_id>[0-9]+)/$', assets_views.detail, name = 'detail'),
     url(r'^$', assets_views.dashboard),
 ]
